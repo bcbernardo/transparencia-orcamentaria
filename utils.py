55c66c698e30337e211c11ad2578eecb071dbbdc
bcbernardo <bernardo.baron@alumni.usp.br>
2019-06-22 22:03:18 -0300

    Fix syntax errors and style.

--- utils.py
+++ utils.py
@@ -2,22 +2,27 @@
 
 
 class JQGrid():
-    """ Models a javascript query grid, with method to scrape its contents """
-    def __init__(self, page):
+    """ Models a javascript query grid, with method to scrape its contents.
+
+    Attributes:
+        page (str): Page source in HTML.
+    """
+
+    def __init__(self, page: str):
         self.page = page
         self.grid = page.xpath(
             "/html/body/div[@id='main_content']/div[div[@id='gbox_list']]")
         self.fields = tuple(f for f in self.grid.xpath(
-            "//thead/tr[@class='ui-jqgrid-labels']/th[@role='columnheader']" + 
-            "/div/text()"))
+            "//thead/tr[@class='ui-jqgrid-labels']/th[@role='columnheader']"
+            + "/div/text()"))
         self.rows = self.grid.xpath(
             "//table[@id='list']/tbody/tr[@id!='jqgfirstrow']")
-        self.pager = page.xpath("/div[@id='gbox_list']/div[@id='pager']/div"
+        self.pager = page.xpath("/div[@id='gbox_list']/div[@id='pager']/div")
         self.page_no = self.pager.xpath("//span[@id='sp_1_pager']/text()")
-        self_rows_per_page = self.pager.xpath(
+        self.rows_per_page = self.pager.xpath(
             "//select/option[@selected='selected']/text()")
         self.last_update = self.grid("/div[@id=data_atualizacao]")
-    
+
     def get_values(self):
         result = dict()
         for r in self.rows:
@@ -33,4 +38,4 @@ class JQGrid():
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit()
